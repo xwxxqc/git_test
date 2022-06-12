fb78e0927f54f52b652e6befe6b65adda9b13b4d
xwxxqc <85996219+xwxxqc@users.noreply.github.com>
2022-06-12 14:27:20 +0800

    Update Main.java

--- src/main/java/com/git/Main.java
+++ src/main/java/com/git/Main.java
@@ -5,6 +5,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
         System.out.println("this is HelloWorld");
+        System.out.println("this is gitHubCode");
     }
 
 }
